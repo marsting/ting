9f3787158efee39e520b15adf11c71d4ec5b634b
chu <t2893919@gmail.com>
2022-07-03 22:35:25 +0800

    Navbar製作

---
+++ src/component/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navbar.css'
+
+function Navbar (){
+    return(
+        <>
+        <div className='nav'>
+        <div className='navLine'>
+            <p className='titleN'> CHU</p>
+            <div className='NBtn'>
+                <div className='pinkN'></div>
+                <div className='yellowN'></div>
+                <div className='blueN'></div>
+            </div>
+        </div>
+        </div>
+        </>
+    )
+}
+export default Navbar

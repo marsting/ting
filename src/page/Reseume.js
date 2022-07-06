5e081d1e96b6da46e8c57759cbe690c84690e555
chu <t2893919@gmail.com>
2022-07-06 14:38:06 +0800

    新增標題照片

---
+++ src/page/Reseume.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Reseume.css'
+
+function Reseume (){
+    return(
+        <>
+        <div className='container'>
+        <img className='Rtitle' src={require('../img/RESEUME.png')} alt="" ></img>
+        <div>
+            
+        </div>
+        </div>
+        </>
+    )
+}
+    export default Reseume

efbd20b8496604622a81f07b232bda58613d7094
chu <t2893919@gmail.com>
2022-07-18 21:32:42 +0800

    首頁製作

--- src/page/home.js
+++ src/page/home.js
@@ -4,10 +4,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
-
 import About from './About';
+import Contact from './Contact';
+import Reseume from './Reseume';
+import Project from './Project';
 import './home.css';
 
+<script crossorigin src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.9/fullpage.min.js"></script>
+<link rel="stylesheet" type="text/css" href="fullpage.css" />
 
 
 
@@ -16,13 +20,12 @@ function Home (){
     return(
         <>
       <About/>
-            <div className='color-change-2x'>
-        
+            {/* <div className='color-change-2x'>    
             <h1 className='tracking-in-expand'>welcome my web</h1>
-       
-
-           </div>
-       
+           </div> */}
+       {/* <Reseume/> */}
+       {/* <Project/> */}
+       {/* <Contact/> */}
         </>
     )
 }
